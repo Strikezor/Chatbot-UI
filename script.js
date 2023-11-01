541ce6507373c6937d63eca2db984f44d527aff9
Akshay Chaturvedi <114054547+Strikezor@users.noreply.github.com>
2023-11-01 15:45:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var faqwindow=document.querySelector('.faq-window');
+const show_hide=()=>{
+    if(faqwindow.style.display==='block'){
+        faqwindow.style.display='none';
+    }
+    else{
+        faqwindow.style.display='block';
+    }
+}
